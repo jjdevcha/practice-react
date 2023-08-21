0d6ec2ef04e35bed69141d7c4f6c47cdc38247ec
Jung In <jungincha923@gmail.com>
2023-08-21 22:02:29 +1000

    useEffect Kata practice

---
+++ useEffect/src/App2.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+
+export default function App2() {
+  //   const [display, setDisplay] = useState("posts");
+  //   const [items, setItems] = useState([]);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  //   useEffect(() => {
+  //     fetch(`https://jsonplaceholder.typicode.com/${display}`)
+  //       .then((response) => response.json())
+  //       .then((json) => setItems(json));
+  //   }, [display]);
+
+  function handleResize() {
+    setScreenWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [screenWidth]);
+
+  return (
+    <>
+      <h1>{screenWidth}</h1>
+      {/* <div>
+        <button onClick={() => setDisplay("posts")}>Posts</button>
+        <button onClick={() => setDisplay("users")}>Users</button>
+        <button onClick={() => setDisplay("comments")}>Comments</button>
+      </div>
+      <h1>{display}</h1>
+      {items.map((item) => {
+        return <p>{JSON.stringify(item)}</p>;
+      })} */}
+    </>
+  );
+}
