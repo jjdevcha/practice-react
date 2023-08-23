d36907443327f22ba17a41ee1763137f4abd4691
Jung In <jungincha923@gmail.com>
2023-08-23 22:03:42 +1000

    React virtual DOM

---
+++ virtualDOM/src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import DemoOutput from "./DemoOutput";
+
+import "./App.css";
+
+function App() {
+  const [showP, setShowP] = useState(false);
+  console.log("App loaded");
+
+  function handleShowP() {
+    setShowP((prev) => !prev);
+  }
+  // if a component is getting re-executed/ re-evaluated
+  // it's child component will get re-executed/ re-evaluated as well no matter what
+  // So sometimes we will have to do sth to prevent unnecessary re-execute/ re-evaluation
+
+  return (
+    <>
+      <h1>Hi there!</h1>
+      <DemoOutput show={showP} />
+      <button onClick={handleShowP}>Click here</button>
+    </>
+  );
+}
+
+export default App;
