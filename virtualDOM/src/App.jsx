c1317112f793366138c2df976e8e371b056a7680
Jung In <jungincha923@gmail.com>
2023-08-25 12:03:07 +1000

    useCallback React

--- virtualDOM/src/App.jsx
+++ virtualDOM/src/App.jsx
@@ -1,23 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import DemoOutput from "./DemoOutput";
 
 import "./App.css";
 
 function App() {
   const [showP, setShowP] = useState(false);
+  const [allowClick, setAllowClick] = useState(true);
   console.log("App loaded");
 
-  function handleShowP() {
-    setShowP((prev) => !prev);
-  }
+  const handleAllowClick = () => {
+    setAllowClick((prev) => !prev);
+  };
+
+  const handleShowP = useCallback(() => {
+    if (allowClick) {
+      setShowP((prev) => !prev);
+    }
+  }, [allowClick]);
   // if a component is getting re-executed/ re-evaluated
   // it's child component will get re-executed/ re-evaluated as well no matter what
   // So sometimes we will have to do sth to prevent unnecessary re-execute/ re-evaluation
 
+  // useCallback() helps the app to know the function won't change (recreated)
+  // so that it won't re-evaluate/re-examinate over and over
+  // But sometimes we need certain values to be updated
+  // so that's why we have to specify the value in [] dependencies array
+
   return (
     <>
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showP} />
+      <button onClick={handleAllowClick}>Allow click</button>
       <button onClick={handleShowP}>Click here</button>
     </>
   );
