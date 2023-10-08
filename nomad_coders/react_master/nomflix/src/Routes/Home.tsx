2cf6dbbc3366d42da339e18aa19906477a84748d
Jung In <jungincha923@gmail.com>
2023-10-08 15:34:39 +1000

    React nomflix modal update

--- nomad_coders/react_master/nomflix/src/Routes/Home.tsx
+++ nomad_coders/react_master/nomflix/src/Routes/Home.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { makeImagePath } from "../utils";
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
+import { useMatch, useNavigate, PathMatch } from "react-router-dom";
 
 const Wrapper = styled.div`
   background: black;
@@ -31,11 +32,13 @@ const Banner = styled.div<{ bgphoto: string }>`
 const Title = styled.h2`
   font-size: 68px;
   margin-bottom: 20px;
+  color: ${(props) => props.theme.white.lighter};
 `;
 
 const Overview = styled.p`
   font-size: 30px;
   width: 50%;
+  color: ${(props) => props.theme.white.lighter};
 `;
 
 const Slider = styled.div`
@@ -51,7 +54,7 @@ const Row = styled(motion.div)`
   width: 100%;
 `;
 
-const Box = styled(motion.div)`
+const Box = styled(motion.div)<{ bgphoto: string }>`
   background-color: white;
   background-image: url(${(props) => props.bgphoto});
   background-size: cover;
@@ -59,6 +62,69 @@ const Box = styled(motion.div)`
   height: 25vh;
   color: red;
   font-size: 66px;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
+`;
+
+const Info = styled(motion.div)`
+  padding: 10px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h4 {
+    text-align: center;
+    font-size: 18px;
+    color: ${(props) => props.theme.white.lighter};
+  }
+`;
+
+const Overlay = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  opacity: 0;
+`;
+
+const BigMovie = styled(motion.div)`
+  position: fixed;
+  top: 100px;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  width: 40vw;
+  height: 80vh;
+  border-radius: 15px;
+  overflow: hidden;
+  background-color: ${(props) => props.theme.black.lighter};
+`;
+
+const BigCover = styled.div`
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  height: 400px;
+`;
+const BigTitle = styled.h3`
+  color: ${(props) => props.theme.white.lighter};
+  padding: 20px;
+  font-size: 46px;
+  position: relative;
+  top: -80px;
+`;
+
+const BigOverview = styled.p`
+  padding: 20px;
+  position: relative;
+  top: -80px;
+  color: ${(props) => props.theme.white.lighter};
 `;
 
 const rowVars = {
@@ -73,9 +139,37 @@ const rowVars = {
   },
 };
 
+const boxVars = {
+  normal: {
+    scale: 1,
+  },
+  hover: {
+    scale: 1.3,
+    y: -50,
+    transition: {
+      delay: 0.3,
+      duration: 0.3,
+      type: "tween",
+    },
+  },
+};
+
+const infoVars = {
+  hover: {
+    opacity: 1,
+    transition: {
+      delay: 0.3,
+      duration: 0.3,
+      type: "tween",
+    },
+  },
+};
+
 const offset = 6; // max box showing on the screen
 
 export default function Home() {
+  const navigate = useNavigate();
+  const moviePathMatch: PathMatch<string> | null = useMatch("/movies/:id");
   const { data, isLoading } = useQuery<IMovieListResponse>({
     queryKey: ["movies", "nowPlaying"],
     queryFn: getMovies,
@@ -92,7 +186,13 @@ export default function Home() {
     }
   };
   const toggleLeaving = () => setLeaving((prev) => !prev);
-
+  const onBoxClicked = (movieId: number) => {
+    navigate(`/movies/${movieId}`);
+  };
+  const onOverlayClick = () => navigate("/");
+  const clickedMovie =
+    moviePathMatch?.params.id &&
+    data?.results.find((movie) => movie.id === +moviePathMatch.params.id);
   return (
     <Wrapper>
       {isLoading ? (
@@ -113,7 +213,7 @@ export default function Home() {
                 initial="hidden"
                 animate="visible"
                 exit="exit"
-                transition={{ type: "tween", duration: 1 }}
+                transition={{ type: "tween", duration: 0.5 }}
                 key={index} // page number
               >
                 {data?.results
@@ -121,13 +221,51 @@ export default function Home() {
                   .slice(offset * index, offset * index + offset)
                   .map((movie) => (
                     <Box
+                      layoutId={movie.id + ""}
                       key={movie.id}
+                      whileHover="hover"
+                      initial="noraml"
+                      variants={boxVars}
+                      transition={{ type: "tween" }}
                       bgphoto={makeImagePath(movie.backdrop_path || "", "w500")}
-                    />
+                      onClick={() => onBoxClicked(movie.id)}
+                    >
+                      <Info variants={infoVars}>
+                        <h4>{movie.title}</h4>
+                      </Info>
+                    </Box>
                   ))}
               </Row>
             </AnimatePresence>
           </Slider>
+          <AnimatePresence>
+            {moviePathMatch ? (
+              <>
+                <Overlay
+                  onClick={onOverlayClick}
+                  exit={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                />
+                <BigMovie layoutId={moviePathMatch.params.id}>
+                  {clickedMovie && (
+                    <>
+                      <BigCover
+                        style={{
+                          backgroundImage: `linear-gradient(to top, black, transparent),
+                        url(${makeImagePath(
+                          clickedMovie.backdrop_path || "",
+                          "w500"
+                        )})`,
+                        }}
+                      />
+                      <BigTitle>{clickedMovie.title}</BigTitle>
+                      <BigOverview>{clickedMovie.overview}</BigOverview>
+                    </>
+                  )}
+                </BigMovie>
+              </>
+            ) : null}
+          </AnimatePresence>
         </>
       )}
     </Wrapper>
