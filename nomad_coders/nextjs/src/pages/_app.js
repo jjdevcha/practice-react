080eca6dbd37b71c7112e0e865073ea982d03aa4
Jung In <jungincha923@gmail.com>
2023-10-12 00:07:30 +1000

    next js layout

--- nomad_coders/nextjs/src/pages/_app.js
+++ nomad_coders/nextjs/src/pages/_app.js
@@ -1,16 +1,16 @@
 import NavBar from "@/components/NavBar";
 import "../styles/globals.css";
+import Layout from "@/components/Layout";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
       <style jsx global>{`
         a {
           color: white;
         }
       `}</style>
-    </>
+    </Layout>
   );
 }
--- nomad_coders/nextjs/src/pages/index.js
+++ nomad_coders/nextjs/src/pages/index.js
@@ -1,9 +1,29 @@
-import NavBar from "@/components/NavBar";
+import Seo from "@/components/Seo";
+import { useEffect, useState } from "react";
+
+const API_KEY = "f40c94992343eda847426e20d7977c2e";
 
 export default function Home() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <>
-      <h1>Home</h1>
+      <Seo title="Home" />
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </>
   );
 }
