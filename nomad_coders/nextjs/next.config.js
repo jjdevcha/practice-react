45127cec788c2d54209724a78b1658bdb52ca8e2
Jung In <jungincha923@gmail.com>
2023-10-13 00:06:29 +1000

    next js rewrite and redirect

--- nomad_coders/nextjs/next.config.js
+++ nomad_coders/nextjs/next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
+const API_KEY = `f40c94992343eda847426e20d7977c2e`;
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
